b720e0fdca44b45cfecc5f49b3e22f6b9a076f38
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-27 22:17:58 +0000

    ServerApp creates new ControlService

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
@@ -1,9 +1,15 @@
 package pt.tecnico.sauron.silo;
 
 
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class SiloServerApp {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.out.println(SiloServerApp.class.getSimpleName());
 		
 		// receive and print arguments
@@ -11,6 +17,22 @@ public class SiloServerApp {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+
+		if (args.length < 1) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s port%n", SiloServerApp.class.getName());
+			return;
+		}
+
+		final int port = Integer.parseInt(args[0]);
+		final BindableService impl = new SiloControlServiceImpl();
+
+		Server server = ServerBuilder.forPort(port).addService(impl).build();
+
+		server.start();
+
+		System.out.println("Server started");
+		server.awaitTermination();
 	}
 	
 }
