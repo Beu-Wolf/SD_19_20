88256e8102d66b9e9402f2a2b23e7b54c480cef1
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-27 22:18:15 +0000

    class for Control commands

---
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloControlServiceImpl.java
@@ -0,0 +1,26 @@
+package pt.tecnico.sauron.silo;
+
+import io.grpc.stub.StreamObserver;
+import static io.grpc.Status.*;
+import pt.tecnico.sauron.silo.grpc.ControlServiceGrpc;
+import pt.tecnico.sauron.silo.grpc.Silo;
+
+public class SiloControlServiceImpl extends ControlServiceGrpc.ControlServiceImplBase {
+
+    @Override
+    public void ping(Silo.PingRequest request, StreamObserver<Silo.PingResponse> responseObserver) {
+        String input = request.getText();
+
+        if (input == null || input.isBlank()) {
+            responseObserver.onError(INVALID_ARGUMENT.withDescription("Input cannot be blank!").asRuntimeException());
+            return;
+        }
+
+        String output = "Hello " + input + "!";
+        Silo.PingResponse response = Silo.PingResponse.newBuilder()
+                .setText(output).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
