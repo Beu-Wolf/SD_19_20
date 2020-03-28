203f3efde6877a4a73d59f807a75520ff6f5c4dc
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-28 13:05:34 +0000

    Handled exceptions in main function

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class SiloServerApp {
 	
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) {
 		System.out.println(SiloServerApp.class.getSimpleName());
 		
 		// receive and print arguments
@@ -29,10 +29,21 @@ public class SiloServerApp {
 
 		Server server = ServerBuilder.forPort(port).addService(impl).build();
 
-		server.start();
+		try {
+			server.start();
+		} catch(IOException e) {
+			System.err.println("Error starting server at port: " + port);
+		}
+
 
 		System.out.println("Server started");
-		server.awaitTermination();
+		try {
+			server.awaitTermination();
+		} catch (InterruptedException e) {
+			System.err.println("Error terminating server at port: " + port);
+			Thread.currentThread().interrupt();
+		}
+
 	}
 	
 }
