3721c804cf366d79479ffd8c6b2e4114e09a7178
Marcelo Santos <marcelocmsantos@gmail.com>
2020-03-28 22:28:20 +0000

    Simplify ErrorMessages

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/exceptions/ErrorMessages.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/exceptions/ErrorMessages.java
@@ -1,17 +1,5 @@
 package pt.tecnico.sauron.silo.domain.exceptions;
 
-public enum ErrorMessages {
-
-    BLANK_INPUT("Input cannot be blank!");
-
-    private String label;
-
-    ErrorMessages(String label) {
-        this.label = label;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
+public class ErrorMessages {
+    public static String BLANK_INPUT = "Input cannot be blank!";
 }
