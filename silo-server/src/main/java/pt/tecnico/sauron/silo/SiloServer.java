b1e842ed8ff190f3a992f17e50109d1c519315d3
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-29 19:01:55 +0100

    SiloServerApp now instantiates a custom SiloServer

---
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServer.java
@@ -0,0 +1,35 @@
+package pt.tecnico.sauron.silo;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import pt.tecnico.sauron.silo.domain.Silo;
+
+import java.io.IOException;
+
+public class SiloServer {
+
+    private final int port;
+    private final Server server;
+    private final Silo silo = new Silo();
+
+    final BindableService controlImpl = new SiloControlServiceImpl();
+
+    public SiloServer(int port){
+        this(ServerBuilder.forPort(port), port);
+    }
+    /** Create a RouteGuide server using serverBuilder as a base and features as data. */
+    public SiloServer(ServerBuilder<?> serverBuilder, int port) {
+        this.port = port;
+        server = serverBuilder.addService(controlImpl).build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        System.out.println("Server started, listening on " + port);
+    }
+
+    public void awaitTermination() throws InterruptedException {
+        server.awaitTermination();
+    }
+}
