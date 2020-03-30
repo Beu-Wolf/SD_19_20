8b830f95679d409466734ba88573407b96d85531
Beu-Wolf <daniel.gseara@gmail.com>
2020-03-30 15:46:11 +0100

    Added countdown latch

--- silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloFrontend.java
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloFrontend.java
@@ -5,13 +5,14 @@ import io.grpc.stub.MetadataUtils;
 import io.grpc.stub.StreamObserver;
 import pt.tecnico.sauron.silo.client.dto.CamDto;
 import pt.tecnico.sauron.silo.client.dto.ObservationDto;
-import pt.tecnico.sauron.silo.client.exceptions.InvalidArgumentException;
 import pt.tecnico.sauron.silo.client.exceptions.TypeNotSupportedException;
 import pt.tecnico.sauron.silo.grpc.ControlServiceGrpc;
 import pt.tecnico.sauron.silo.grpc.ReportServiceGrpc;
 import pt.tecnico.sauron.silo.grpc.Silo;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class SiloFrontend {
     private ManagedChannel channel;
@@ -30,26 +31,34 @@ public class SiloFrontend {
 
     public CamDto camInfo(String name) { return null; }
 
-    public void report(String name, List<ObservationDto> observations) throws TypeNotSupportedException, InvalidArgumentException{
+    public void report(String name, List<ObservationDto> observations) throws TypeNotSupportedException, InterruptedException {
         Metadata header = new Metadata();
         header.put(METADATA_CAM_NAME, name);
-        reportStub = MetadataUtils.attachHeaders(reportStub, header);
+        this.reportStub = MetadataUtils.attachHeaders(this.reportStub, header);
+        final CountDownLatch latch = new CountDownLatch(1);
 
-        StreamObserver<Silo.ReportResponse> reportObserver =  new StreamObserver<>() {
+        System.out.println(observations.toString());
+
+        StreamObserver<Silo.ReportResponse> responseObserver =  new StreamObserver<>() {
             @Override
             public void onNext(Silo.ReportResponse reportResponse) { }
+
             @Override
             public void onError(Throwable throwable) {
-                System.err.println("Error while reporting!");
+                Status status = Status.fromThrowable(throwable);
+                if(status.getCode() == Status.Code.NOT_FOUND || status.getCode() == Status.Code.INVALID_ARGUMENT) {
+                    System.err.println(status.getDescription());
+                    latch.countDown();
+                }
             }
             @Override
             public void onCompleted() {
                 System.out.println("Successfully reported observations!");
+                latch.countDown();
             }
         };
-
+        StreamObserver<Silo.Observation> requestObserver = this.reportStub.report(responseObserver);
         try {
-            StreamObserver<Silo.Observation> observationObserver = reportStub.report(reportObserver);
             for (ObservationDto observationDto : observations) {
                 ObservationDto.ObservationType type = observationDto.getType();
                 Silo.ObservationType observationType;
@@ -63,17 +72,20 @@ public class SiloFrontend {
                     default:
                         throw new TypeNotSupportedException();
                 }
+                System.out.println(observationDto.toString());
 
                 Silo.Observation observation = Silo.Observation.newBuilder().setObservationId(observationDto.getId())
                         .setType(observationType).build();
-                observationObserver.onNext(observation);
+                requestObserver.onNext(observation);
+                Thread.sleep(10);
             }
-            observationObserver.onCompleted();
-        } catch (StatusRuntimeException e) {
-            if(e.getStatus() == Status.INVALID_ARGUMENT) {
-                throw new InvalidArgumentException(e.getStatus().getDescription());
-            }
+        } catch (RuntimeException e) {
+            requestObserver.onError(e);
+            System.out.println("Error while sending reports" + e.toString());
         }
+        requestObserver.onCompleted();
+        latch.await(10, TimeUnit.SECONDS);
+
 
 
     }
@@ -86,7 +98,7 @@ public class SiloFrontend {
 
     public String ctrlPing(String sentence) {
         Silo.PingRequest request = Silo.PingRequest.newBuilder().setText(sentence).build();
-        Silo.PingResponse response = ctrlStub.ping(request);
+        Silo.PingResponse response = this.ctrlStub.ping(request);
         return response.getText();
     }
 
@@ -95,6 +107,6 @@ public class SiloFrontend {
     public void ctrlInit() {}
 
     public void shutdown() {
-        channel.shutdown();
+        this.channel.shutdown();
     }
 }
