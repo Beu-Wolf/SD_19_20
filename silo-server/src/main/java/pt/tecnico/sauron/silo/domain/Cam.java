a787202b445cc08831070fb95fce0696eb860d56
afonsocrg <afonso.crg@gmail.com>
2020-03-31 16:03:07 +0100

    define comparator

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Cam.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Cam.java
@@ -19,4 +19,13 @@ public class Cam {
 
     public Double getLat() { return this.coords.getLat(); }
     public Double getLon() { return this.coords.getLon(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Cam) {
+            Cam c = (Cam)o;
+            return c.getName() == this.name && this.coords.equals(c.getCoords());
+        }
+        return false;
+    }
 }
