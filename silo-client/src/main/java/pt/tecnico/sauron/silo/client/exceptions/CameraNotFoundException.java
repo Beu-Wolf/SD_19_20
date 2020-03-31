dbfbaec39f16f2e3209f8865ddc27dcc7d80279f
afonsocrg <afonso.crg@gmail.com>
2020-03-31 16:04:28 +0100

    add new error messages and exceptions

---
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/exceptions/CameraNotFoundException.java
@@ -0,0 +1,5 @@
+package pt.tecnico.sauron.silo.client.exceptions;
+
+public class CameraNotFoundException extends FrontendException {
+    public CameraNotFoundException(String message) { super(message); }
+}
--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/exceptions/ErrorMessages.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/exceptions/ErrorMessages.java
@@ -5,5 +5,6 @@ public class ErrorMessages {
     public static String INVALID_PERSON_ID = "Person ID must be an unsigned long!";
     public static String INVALID_CAR_ID = "Car ID must be a valid portuguese license plate!";
     public static String NO_CAM_FOUND = "Camera not found!";
+    public static String DUPLICATE_CAMERA_NAME_EXCEPTION = "There already exists a camera with that name!";
     public static String TYPE_NOT_SUPPORTED = "Type to observe not supported!";
 }
