069e6ca15c536adb12799ea5c9216b68e25356ae
Beu-Wolf <daniel.gseara@gmail.com>
2020-04-01 11:36:32 +0100

    Changed messages to final

--- silo-client/src/main/java/pt/tecnico/sauron/silo/client/exceptions/ErrorMessages.java
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/exceptions/ErrorMessages.java
@@ -1,13 +1,13 @@
 package pt.tecnico.sauron.silo.client.exceptions;
 
 public class ErrorMessages {
-    public static String BLANK_PING_INPUT = "Input cannot be blank!";
-    public static String CAMERA_ALREADY_EXISTS = "Camera name already exists in other location";
-    public static String CAMERA_NOT_FOUND = "Camera not found";
-    public static String FAILED_TO_REGISTER_CAMERA = "Could not register camera";
-    public static String FAILED_TO_RETRIEVE_CAMERA_INFO = "Could not retrieve camera info";
-    public static String TYPE_NOT_SUPPORTED = "Type to observe not supported!";
-    public static String WAITING_THREAD_INTERRUPT = "Waiting for the thread to interrupt!";
-    public static String GENERIC_QUERY_ERROR = "Error executing query!";
-    public static String OBSERVATION_NOT_FOUND = "Observation not found!";
+    public static final String BLANK_PING_INPUT = "Input cannot be blank!";
+    public static final String CAMERA_ALREADY_EXISTS = "Camera name already exists in other location";
+    public static final String CAMERA_NOT_FOUND = "Camera not found";
+    public static final String FAILED_TO_REGISTER_CAMERA = "Could not register camera";
+    public static final String FAILED_TO_RETRIEVE_CAMERA_INFO = "Could not retrieve camera info";
+    public static final String TYPE_NOT_SUPPORTED = "Type to observe not supported!";
+    public static final String WAITING_THREAD_INTERRUPT = "Waiting for the thread to interrupt!";
+    public static final String GENERIC_QUERY_ERROR = "Error executing query!";
+    public static final String OBSERVATION_NOT_FOUND = "Observation not found!";
 }
