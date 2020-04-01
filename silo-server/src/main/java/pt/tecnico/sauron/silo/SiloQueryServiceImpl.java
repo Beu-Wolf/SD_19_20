c2a649280a1b91af1da36228172d3091da140911
Marcelo Santos <marcelocmsantos@gmail.com>
2020-04-01 13:45:09 +0100

    Implement server-side trackMatch

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloQueryServiceImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloQueryServiceImpl.java
@@ -2,6 +2,7 @@ package pt.tecnico.sauron.silo;
 
 import com.google.protobuf.Timestamp;
 import com.google.type.LatLng;
+import io.grpc.StatusException;
 import io.grpc.stub.StreamObserver;
 import io.grpc.Status;
 import pt.tecnico.sauron.silo.domain.*;
@@ -12,6 +13,11 @@ import pt.tecnico.sauron.silo.grpc.QueryServiceGrpc;
 import pt.tecnico.sauron.silo.grpc.Silo.QueryRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.QueryResponse;
 import pt.tecnico.sauron.silo.grpc.Silo.ObservationType;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 public class SiloQueryServiceImpl extends QueryServiceGrpc.QueryServiceImplBase {
     private Silo silo;
@@ -59,19 +65,62 @@ public class SiloQueryServiceImpl extends QueryServiceGrpc.QueryServiceImplBase 
         }
     }
 
+    @Override
+    public void trackMatch(QueryRequest request, StreamObserver<QueryResponse> responseObserver) {
+        ObservationType type = request.getType();
+        String pattern = request.getId();
+
+        TreeSet<String> matched = new TreeSet<>();
+
+        pattern = Pattern.quote(pattern);
+        pattern = pattern.replace("*", "\\E.*\\Q");
+        Pattern p = Pattern.compile(pattern);
+
+        for (Report report : silo.getReports()) {
+            Observation observation = report.getObservation();
+            String id = observation.getId();
+
+            try {
+                if (domainObservationToTypeGRPC(observation) == request.getType() &&
+                        !matched.contains(id) &&
+                        p.matcher(id).find()) {
+                    matched.add(id);
+
+                    responseObserver.onNext(domainReportToGRPC(report));
+                }
+            } catch (SiloInvalidArgumentException e) {}
+        }
+
+        if (matched.isEmpty()) {
+            responseObserver.onError(Status.NOT_FOUND.asException());
+        } else {
+            responseObserver.onCompleted();
+        }
+    }
+
+    private QueryResponse domainReportToGRPC(Report report) throws SiloInvalidArgumentException {
+        return QueryResponse.newBuilder()
+                .setTimestamp(Timestamp.newBuilder().setSeconds(report.getTimestamp().getEpochSecond()))
+                .setObservation(domainObservationToGRPC(report.getObservation()))
+                .setCam(domainCamToGRPC(report.getCam())).build();
+    }
+
     private pt.tecnico.sauron.silo.grpc.Silo.Observation domainObservationToGRPC(Observation observation)
             throws SiloInvalidArgumentException {
-        ObservationType observationType;
+        return pt.tecnico.sauron.silo.grpc.Silo.Observation.newBuilder()
+                .setType(domainObservationToTypeGRPC(observation))
+                .setObservationId(observation.getId()).build();
+    }
+
+    private ObservationType domainObservationToTypeGRPC(Observation observation)
+        throws SiloInvalidArgumentException {
         if (observation instanceof Car) {
-            observationType = ObservationType.CAR;
+            return ObservationType.CAR;
         } else if (observation instanceof Person) {
-            observationType = ObservationType.PERSON;
+            return ObservationType.PERSON;
         } else {
             throw new SiloInvalidArgumentException(ErrorMessages.UNIMPLEMENTED_OBSERVATION_TYPE);
         }
-
-        return pt.tecnico.sauron.silo.grpc.Silo.Observation.newBuilder().setType(observationType)
-                .setObservationId(observation.getId()).build();
     }
 
     private pt.tecnico.sauron.silo.grpc.Silo.Cam domainCamToGRPC(Cam cam) {
