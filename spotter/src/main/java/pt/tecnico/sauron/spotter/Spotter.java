7ae9ba2566349dd74250cb683d4cc52e3d1b7f29
Beu-Wolf <daniel.gseara@gmail.com>
2020-04-02 14:02:08 +0100

    Called implemented methods

--- spotter/src/main/java/pt/tecnico/sauron/spotter/Spotter.java
+++ spotter/src/main/java/pt/tecnico/sauron/spotter/Spotter.java
@@ -6,27 +6,28 @@ import pt.tecnico.sauron.silo.client.dto.ObservationDto;
 import pt.tecnico.sauron.silo.client.dto.ReportDto;
 import pt.tecnico.sauron.silo.client.exceptions.QueryException;
 
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Spotter {
     private SiloFrontend siloFrontend;
+    List<ReportDto> reportOrderedList = new LinkedList<>();
 
     // commands
-    private final String SPOT_CAR = "^spot car (\\w+)$";
-    private final String SPOT_PERSON = "^spot person (\\w+)$";
-    private final String SPOT_CAR_PARTIAL = "^spot car ([\\w*]+)$";
-    private final String SPOT_PERSON__PARTIAL = "^spot person ([\\w*]+)$";
-    private final String TRACE_CAR = "^trail car (\\w+)$";
-    private final String TRACE_PERSON = "^trail person (\\w+)$";
-    private final String EXIT = "^exit$";
+    private static final String SPOT_CAR = "^spot car (\\w+)$";
+    private static final String SPOT_PERSON = "^spot person (\\w+)$";
+    private static final String SPOT_CAR_PARTIAL = "^spot car ([\\w*]+)$";
+    private static final String SPOT_PERSON_PARTIAL = "^spot person ([\\w*]+)$";
+    private static final String TRACE_CAR = "^trail car (\\w+)$";
+    private static final String TRACE_PERSON = "^trail person (\\w+)$";
+    private static final String EXIT = "^exit$";
 
     // patterns
     private final Pattern spotCar = Pattern.compile(SPOT_CAR);
     private final Pattern spotPerson = Pattern.compile(SPOT_PERSON);
     private final Pattern spotCarPartial = Pattern.compile(SPOT_CAR_PARTIAL);
-    private final Pattern spotPersonPartial = Pattern.compile(SPOT_PERSON__PARTIAL);
+    private final Pattern spotPersonPartial = Pattern.compile(SPOT_PERSON_PARTIAL);
     private final Pattern traceCar = Pattern.compile(TRACE_CAR);
     private final Pattern tracePerson = Pattern.compile(TRACE_PERSON);
 
@@ -55,16 +56,20 @@ public class Spotter {
                     System.out.println(reportDto);
                 } else if (Pattern.matches(SPOT_CAR_PARTIAL, command)) {
                     String id = getIdfromPattern(command, spotCarPartial, 1);
-                    //siloFrontend.trackMatch(ObservationDto.ObservationType.Car, id, lambda));
-                } else if (Pattern.matches(SPOT_PERSON__PARTIAL, command)) {
+                    Iterator<ReportDto> it = siloFrontend.trackMatch(ObservationDto.ObservationType.CAR, id);
+                    showReports(it, true);
+                } else if (Pattern.matches(SPOT_PERSON_PARTIAL, command)) {
                     String id = getIdfromPattern(command, spotPersonPartial, 1);
-                    //siloFrontend.trackMatch(ObservationDto.ObservationType.Person, id, lambda));
+                    Iterator<ReportDto> it = siloFrontend.trackMatch(ObservationDto.ObservationType.PERSON, id);
+                    showReports(it, true);
                 } else if (Pattern.matches(TRACE_CAR, command)) {
                     String id = getIdfromPattern(command, traceCar, 1);
-                    //siloFrontend.trace(ObservationDto.ObservationType.Car, id, lambda));
+                    Iterator<ReportDto> it = siloFrontend.trace(ObservationDto.ObservationType.CAR, id);
+                    showReports(it, false);
                 } else if (Pattern.matches(TRACE_PERSON, command)) {
                     String id = getIdfromPattern(command, tracePerson, 1);
-                    //siloFrontend.trace(ObservationDto.ObservationType.Person, id, lambda));
+                    Iterator<ReportDto> it = siloFrontend.trace(ObservationDto.ObservationType.PERSON, id);
+                    showReports(it, false);
                 } else {
                     System.out.println("Unrecognized command, try again");
                 }
@@ -83,5 +88,18 @@ public class Spotter {
         Matcher m = pattern.matcher(command);
         m.find();
         return m.group(index);
+    }
+
+    private void showReports(Iterator<ReportDto> it, boolean orderId) {
+        if(orderId) {
+            it.forEachRemaining(reportOrderedList::add);
+            Collections.sort(reportOrderedList);
+            for(ReportDto reportDto: reportOrderedList) {
+                System.out.println(reportDto.toString());
+            }
+        }
+        while(it.hasNext()) {
+            System.out.println(it.next().toString());
+        }
     }
 }
