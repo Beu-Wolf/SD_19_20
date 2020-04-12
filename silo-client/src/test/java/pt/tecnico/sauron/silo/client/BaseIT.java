8209365c23d7a75f67c594ec5183cb384b02d27f
afonsocrg <afonso.crg@gmail.com>
2020-04-12 18:08:45 +0100

    use zoo in IT

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/BaseIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/BaseIT.java
@@ -2,6 +2,7 @@ package pt.tecnico.sauron.silo.client;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import pt.ulisboa.tecnico.sdis.zk.ZKNamingException;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -21,13 +22,14 @@ public class BaseIT {
 			testProps.load(BaseIT.class.getResourceAsStream(TEST_PROP_FILE));
 			System.out.println("Test properties:");
 			System.out.println(testProps);
+			siloFrontend = new SiloFrontend(testProps.getProperty("zoo.host"), testProps.getProperty("zoo.port"), testProps.getProperty("server.path"));
 		}catch (IOException e) {
 			final String msg = String.format("Could not load properties file {}", TEST_PROP_FILE);
 			System.out.println(msg);
 			throw e;
+		} catch (ZKNamingException e) {
+			System.err.println("Could not create frontend: error in lookup");
 		}
-
-		siloFrontend = new SiloFrontend(testProps.getProperty("server.host"), Integer.parseInt(testProps.getProperty("server.port")));
 	}
 	
 	@AfterAll
