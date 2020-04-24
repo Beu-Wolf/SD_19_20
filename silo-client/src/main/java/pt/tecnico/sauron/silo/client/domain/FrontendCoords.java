2dcd22283e223d89eea54bf96d89cf8549156c87
afonsocrg <afonso.crg@gmail.com>
2020-04-24 16:03:44 +0100

    rename dto to domain

---
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/domain/FrontendCoords.java
@@ -0,0 +1,28 @@
+package pt.tecnico.sauron.silo.client.domain;
+
+public class FrontendCoords {
+    double lat;
+    double lon;
+
+    public FrontendCoords(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof FrontendCoords) {
+            FrontendCoords c = (FrontendCoords) o;
+            return getLat() == c.getLat() && getLon() == c.getLon();
+        }
+        return false;
+    }
+}
