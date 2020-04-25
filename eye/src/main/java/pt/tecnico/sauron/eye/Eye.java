3022a08c57764e442afb54ebf467e233c3b825de
afonsocrg <afonso.crg@gmail.com>
2020-04-25 11:34:05 +0100

    change Eye error displaying method

--- eye/src/main/java/pt/tecnico/sauron/eye/Eye.java
+++ eye/src/main/java/pt/tecnico/sauron/eye/Eye.java
@@ -92,10 +92,10 @@ public class Eye {
     private void sendObservations() {
         if(observationBuffer.size() > 0) {
             try {
-                this.siloFrontend.report(this.cam.getName(), observationBuffer);
-                System.out.println("Successfully reported observations!");
+                int numAcked = this.siloFrontend.report(this.cam.getName(), observationBuffer);
+                System.out.printf("Successfully reported %d observations!%n", numAcked);
             } catch (FrontendException e) {
-                System.err.println("Could not add observations: " + e.getMessage());
+                System.err.println("Could not add all observations:\n" + e.getMessage());
             }
         }
     }
