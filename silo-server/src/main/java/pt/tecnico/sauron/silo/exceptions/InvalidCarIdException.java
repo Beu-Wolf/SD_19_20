1bebd0edd60ac65de01f90596b46f4f761bc5571
afonsocrg <afonso.crg@gmail.com>
2020-04-25 13:10:05 +0100

    send ids that caused error

--- silo-server/src/main/java/pt/tecnico/sauron/silo/exceptions/InvalidCarIdException.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/exceptions/InvalidCarIdException.java
@@ -4,4 +4,8 @@ public class InvalidCarIdException extends SiloInvalidArgumentException {
     public InvalidCarIdException() {
         super(ErrorMessages.INVALID_CAR_ID);
     }
+
+    public InvalidCarIdException(String invalidId) {
+        super(invalidId + ": " + ErrorMessages.INVALID_CAR_ID);
+    }
 }
